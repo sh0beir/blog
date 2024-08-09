5b0c24145c71e79c4863c54a1fe814492a0c53f5
Sh0beir <Shobeir.fatehi@gmail.com>
2024-08-09 09:58:30 +0330

    add category relation

--- app/Blog.php
+++ app/Blog.php
@@ -3,8 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Blog extends Model
 {
     //
+    protected $guarded = [];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
